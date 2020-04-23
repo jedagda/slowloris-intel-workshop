de32c350a5585df8d532489c0e68c5f3aff54e81
Josh <jedagda@gmail.com>
2020-04-23 15:16:18 -0600

    Okay, I think it works

--- dedsec/slowloris/scripts/script1.py
+++ dedsec/slowloris/scripts/script1.py
@@ -1,9 +1,9 @@
 import requests
 
 def main():
-    site = ‘http://INSERT IP ADDRESS THAT YOU ANSWERED FROM STEP 8’
+    site = 'http://INSERT IP ADDRESS THAT YOU ANSWERED FROM STEP 8'
     r = requests.get(site)
     print(r.text)
 
-if __name__ ==’__main__’:
+if __name__ =='__main__':
     main()
